98b81443664589cd2eea18e80c360237f7a3d222
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-04-14 23:31:45 +0530

    added: line length calculation

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,7 +1,37 @@
 package com.bridgelabz;
-
+import java.util.Scanner;
 public class LineComparison {
+
+    static Scanner s = new Scanner(System.in);
+    int x1,x2,y1,y2;
+    void getData(){
+        System.out.println("Enter x1:");
+        x1 = s.nextInt();
+        System.out.println("Enter y1:");
+        y1 = s.nextInt();
+        System.out.println("Enter x2:");
+        x2 = s.nextInt();
+        System.out.println("Enter y2:");
+        y2 = s.nextInt();
+    }
+    float getSqrt(){
+        int x = (x2-x1)*(x2-x1);
+        int y = (y2-y1)*(y2-y1);
+        int num = x+y;
+        System.out.println(num);
+        float temp;
+        float sqrt = num/2;
+        do{
+            temp = sqrt;
+            sqrt = (temp+(num/temp))/2;
+        }while ((temp - sqrt) != 0);
+        return sqrt;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison computation Program.");
+
+        LineComparison l = new LineComparison();
+        l.getData();
+        float ans = l.getSqrt();
+        System.out.println("Length of a line is "+ans);
     }
 }
